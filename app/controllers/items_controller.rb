2561a053b3544ffc32045b47eb366d25f2ba8a20
Jae Easley <jaesley@gmail.com>
2017-11-08 15:54:17 -0800

    Add support for nil search queries to Item index

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,12 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    search = params[:search].downcase
-    sanitized_search = search.gsub(/[%_]/, '\\\\\0')
-    @items = Item.where(Item.arel_table[:title].lower.matches("%#{sanitized_search}%").or(Item.arel_table[:barcode].matches("%#{sanitized_search}%")))
+    if params[:search] != nil
+      search = params[:search].downcase
+      sanitized_search = search.gsub(/[%_]/, '\\\\\0')
+      @items = Item.where(Item.arel_table[:title].lower.matches("%#{sanitized_search}%").or(Item.arel_table[:barcode].matches("%#{sanitized_search}%")))
+    else
+      @items = Item.all
+    end
   end
 end
