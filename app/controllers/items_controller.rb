f81831f7452b3eaedcf8e2fb758862a0e61dd3c3
Jae Easley <jaesley@gmail.com>
2017-11-10 10:59:31 -0800

    Remove redundant code

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    if params[:search] != nil
+    if params[:search]
       search = params[:search].downcase
       sanitized_search = search.gsub(/[%_]/, '\\\\\0')
       @items = Item.where(Item.arel_table[:title].lower.matches("%#{sanitized_search}%").or(Item.arel_table[:barcode].matches("%#{sanitized_search}%")))
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    if params[:search] != nil
+    if params[:search]
       search = params[:search].downcase
       sanitized_search = search.gsub(/[%_]/, '\\\\\0')
       @users = User.where(User.arel_table[:email].lower.matches("%#{sanitized_search}%"))
